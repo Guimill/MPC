85b9801dc3a0de74e4494a021d3c4b3f63395a85
Guimill <guillaume.milleret@gmail.com>
2023-04-07 18:00:27 +0200

    some changes in style

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import insta from '../../assets/img/instagram.png'
+import tiktok from '../../assets/img/tiktok.png'
 
 export default function Navbar() {
 
     return (
         <div className="navBar">
             <ul className="navBar__list">
+                <li><a href="t"><img src={insta} alt="" /></a></li>
+                <li><a href="t"><img src={tiktok} alt="" /></a></li>
                 <li><Link to="articles"><button>Articles</button></Link></li>
                 <li><Link to="qsj"><button>Qui suis-je</button></Link></li>
                 <li><Link to="philosophie"><button>Philosophie</button></Link></li>
